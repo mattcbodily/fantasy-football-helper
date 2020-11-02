9be0cc09796e36ba01186c2b2d00eac8cd5ca526
mattcbodily <mattcbodily@gmail.com>
2020-11-02 16:01:59 -0700

    players can now input player fantasy cost, projected points, and team over/under

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Player.css';
 
-export default props => (
-    <section className='player'>
-        <p>{props.player.name}</p>
-        <p>{props.player.team}</p>
-    </section>
-)
+export default props => {
+    let [cost, setCost] = useState(''),
+        [projPts, setProjPts] = useState(''),
+        [teamOvrUnd, setTeamOvrUnd] = useState('');
+
+    return (
+        <section className='player'>
+            <p className='player-name'>{props.player.name}</p>
+            <p className='player-team'>{props.player.team}</p>
+            <input value={cost} placeholder='Cost' onChange={e => setCost(e.target.value)}/>
+            <input value={projPts} placeholder='Projected Points' onChange={e => setProjPts(e.target.value)}/>
+            <input value={teamOvrUnd} placeholder='Team Over/Under' onChange={e => setTeamOvrUnd(e.target.value)}/>
+        </section>
+    )
+}
