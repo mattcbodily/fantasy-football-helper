163a250588a6945875ec78e10bd78b82f6f6570d
mattcbodily <mattcbodily@gmail.com>
2020-11-03 15:53:00 -0700

    added styling to PlayerList and TeamBuilder

--- src/Components/TeamBuilder/TeamBuilder.js
+++ src/Components/TeamBuilder/TeamBuilder.js
@@ -3,10 +3,9 @@ import {connect} from 'react-redux';
 import './TeamBuilder.css';
 
 const TeamBuilder = props => {
-    let [salary, setSalary] = useState(''),
-        [targetValue, setTargetValue] = useState(''),
-        [actualValue, setActualValue] = useState(''),
-        [editView, setEditView] = useState(true);
+    let [salary, setSalary] = useState(50000),
+        [targetValue, setTargetValue] = useState(2.5),
+        [actualValue, setActualValue] = useState(0);
 
     useEffect(() => {
         if(salary){
@@ -24,39 +23,26 @@ const TeamBuilder = props => {
     return (
         <section className='team-builder'>
             <section>
-                {editView
-                ? (
-                    <>
-                        <input value={salary} placeholder='Salary' onChange={e => setSalary(e.target.value)}/>
-                        <input value={targetValue} placeholder='Target Value' onChange={e => setTargetValue(e.target.value)}/>
-                        <button onClick={() => setEditView(false)}>Submit</button>
-                    </>
-                )
-                : (
-                    <>
-                        <span>${salary}</span>
-                        <span>{targetValue}</span>
-                        <span>{actualValue}</span>
-                        <button onClick={() => setEditView(true)}>Edit</button>
-                    </>
-                )}
+                <span className='salary'>Salary: ${salary}</span>
+                <span className='target-value'>Target Value: {targetValue}</span>
+                <span className='actual-value'>Actual Value: {actualValue}</span>
                 {props.team.filter(e => e.position === 'QB').map(player => (
-                    <div>QB: {player.name}</div>
+                    <div className='player'>QB: {player.name}</div>
                 ))}
                 {props.team.filter(e => e.position === 'RB').map(player => (
-                    <div>RB: {player.name}</div>
+                    <div className='player'>RB: {player.name}</div>
                 ))}
                 {props.team.filter(e => e.position === 'WR').map(player => (
-                    <div>WR: {player.name}</div>
+                    <div className='player'>WR: {player.name}</div>
                 ))}
                 {props.team.filter(e => e.position === 'TE').map(player => (
-                    <div>TE: {player.name}</div>
+                    <div className='player'>TE: {player.name}</div>
                 ))}
                 {props.team.filter(e => e.position === 'FLEX').map(player => (
-                    <div>FLEX: {player.name}</div>
+                    <div className='player'>FLEX: {player.name}</div>
                 ))}
                 {props.team.filter(e => e.position === 'DEF').map(player => (
-                    <div>DEF: {player.name}</div>
+                    <div className='player'>DEF: {player.name}</div>
                 ))}
             </section>
         </section>
